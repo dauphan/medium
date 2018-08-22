24ea361ebc2f68cf092833b98d6e6b0f2a24a0d1
Dau Phan Van <dau.phan@mbp-0104.local>
2018-08-22 15:58:08 +0700

    Change index.js file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,36 +1,7 @@
-// server/routes/article.js
-const articlecontroller = require('./../controllers/article.ctrl')
-const multipart = require('connect-multiparty')
-const multipartWare = multipart()
+// server/routes/index.js
+const user = require('./user')
+const article = require('./article')
 module.exports = (router) => {
-    /**
-     * get all articles
-     */
-    router
-        .route('/articles')
-        .get(articlecontroller.getAll)
-    /**
-     * add an article
-     */
-    router
-        .route('/article')
-        .post(multipartWare, articlecontroller.addArticle)
-    /**
-     * clap on an article
-     */
-    router
-        .route('/article/clap')
-        .post(articlecontroller.clapArticle)
-    /**
-     * comment on an article
-     */
-    router
-        .route('/article/comment')
-        .post(articlecontroller.commentArticle)
-    /**
-     * get a particlular article to view
-     */
-    router
-        .route('/article/:id')
-        .get(articlecontroller.getArticle)
+    user(router)
+    article(router)
 }
